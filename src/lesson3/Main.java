dac5f0f7d4fc14e164cbc3f5aa8535b21466e254
wiktuar <wiktuar@yandex.ru>
2023-07-03 12:40:58 +0300

    Reversed List

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,47 +1,109 @@
 package lesson3;
 
-import java.util.Arrays;
+import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {7, 13, 25, -5, 40, 11};
-        heapSort(arr);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        SingleLinkList<Contact> contactList = new SingleLinkList<>();
+
+        contactList.addToEnd(new Contact(123, "Васильев Евстахий Борисович", "+129381832"));
+        contactList.addToEnd(new Contact(151, "Коновалов Степан Петрович", "+234432334"));
+        contactList.addToEnd(new Contact(332, "Калинин Артём Валериевич", "+2234234423"));
+        contactList.addToEnd(new Contact(432, "Предыбайло Григорий Анатолиевич", "+2342344234"));
+        contactList.addToEnd(new Contact(556, "Степанов Мирослав Андреевич", "+6678877777"));
+
+        for(Contact contact: contactList) {
+            System.out.println(contact);
+        }
+
+        contactList.reverse();
+
+        System.out.println("------------------------");
+
+        for(Contact contact: contactList) {
+            System.out.println(contact);
         }
     }
 
-    private static void heapSort(int[] arr){
-        int n = arr.length;
-        for(int i = n/2 -1; i >=0; i--){
-            heapify(arr, i, n);
+    public static class SingleLinkList<T> implements Iterable<T> {
+
+        ListItem<T> head;
+        ListItem<T> tail;
+
+        @Override
+        public Iterator<T> iterator() {
+            return new Iterator<T>() {
+                ListItem<T> current = head;
+
+                @Override
+                public boolean hasNext() {
+                    return current != null;
+                }
+
+                @Override
+                public T next() {
+                    T data = current.data;
+                    current = current.next;
+                    return data;
+                }
+            };
         }
 
-        for(int i = n-1; i>=0; i--){
-            int temp = arr[i];
-            arr[i] = arr[0];
-            arr[0] = temp;
+        private static class ListItem<T> {
+            T data;
+            ListItem<T> next;
+        }
 
-            heapify(arr, 0, i);
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void addToEnd(T item) {
+            ListItem<T> newItem = new ListItem<>();
+            newItem.data = item;
+            if (isEmpty()) {
+                head = newItem;
+                tail = newItem;
+            } else {
+                tail.next = newItem;
+                tail = newItem;
+            }
+        }
+
+        public void reverse() {
+            if (!isEmpty() && head.next != null) {
+                tail = head;
+                ListItem<T> current = head.next;
+                head.next = null;
+                while (current != null) {
+                    ListItem<T> next = current.next;
+                    current.next = head;
+                    head = current;
+                    current = next;
+                }
+            }
         }
     }
 
-    private static void heapify(int[] arr, int i, int n){
-        int l = i * 2 + 1;
-        int r = i * 2 + 2;
-        int largest = i;
+    static class Contact {
+        int id;
+        String name;
+        String phone;
 
-        if(l < n && arr[l] > arr[largest])
-            largest = l;
-        if(r < n && arr[r] > arr[largest])
-            largest = r;
+        public Contact(int id, String name, String phone) {
+            this.id = id;
+            this.name = name;
+            this.phone = phone;
+        }
 
-        if(i != largest){
-            int temp = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = temp;
-
-            heapify(arr, largest, n);
+        @Override
+        public String toString() {
+            return "Contact{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", phone='" + phone + '\'' +
+                    '}';
         }
     }
 }
+
